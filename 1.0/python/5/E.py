ca7365cb716bd877ba94192d646ea0e8c0aeb39a
YxTiBlya <karasevstepan674@gmail.com>
2023-03-17 02:40:44 +0400

    solution for 5 lesson

---
+++ 1.0/python/5/E.py
@@ -0,0 +1,26 @@
+n, k = map(int, input().split())
+colors = list(map(int, input().split()))
+left = right = 0
+aleft, aright = 0, n
+ans = numsInWindow = 0
+dt = {}
+
+while right < n:
+    if dt.get(colors[right]) == None:
+        dt[colors[right]] = 0
+    dt[colors[right]] += 1
+    
+    if dt[colors[right]] == 1:
+        numsInWindow += 1
+    right += 1
+
+    while numsInWindow == k:
+        dt[colors[left]] -= 1
+        if dt[colors[left]] == 0:
+            numsInWindow -= 1
+        left += 1
+        
+        if right - left < aright - aleft:
+            aleft, aright = left, right
+
+print(aleft, aright)
